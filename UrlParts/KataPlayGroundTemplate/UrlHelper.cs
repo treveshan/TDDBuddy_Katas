7f1b7e56d10ca736f33a22451ed720a4dd9e6195
Treveshan Naidoo <treveshan.naidoo@chillisoft.co.za>
2018-05-30 22:00:48 +0200

    Completed UrlParts Kata

---
+++ UrlParts/KataPlayGroundTemplate/UrlHelper.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KataPlayGroundTemplate
+{
+    public class UrlHelper
+    {
+        private readonly Dictionary<string, string> _defaultPorts = new Dictionary<string, string>()
+        {
+            {"http","80" },
+            {"https","443" },
+            {"ftp","21" },
+            {"ftps","22" }
+        };
+
+        public UrlParts GetUrlParts(string url)
+        {
+            return new UrlParts()
+            {
+                Protocol = GetProtocol(url),
+                Subdomain = GetSubdomain(url),
+                Domain = GetDomain(url),
+                Port = GetPort(url),
+                Path = GetPath(url)
+            };
+        }
+
+        private string GetPath(string url)
+        {
+            var numberOfPathDelimiters = url.Count(c => c.Equals('/'));
+            var path = numberOfPathDelimiters <= 2 ? string.Empty : GetPathOfUrlWhenPathExists(url);
+            return path;
+        }
+
+        private string GetPathOfUrlWhenPathExists(string url)
+        {
+            var urlWithOutProtocolDelimilter = url.Replace("://", "");
+            var lastIndexOfPathDelimiter = urlWithOutProtocolDelimilter.IndexOf("/", StringComparison.CurrentCultureIgnoreCase);
+            var lenghtOfPathOnly = urlWithOutProtocolDelimilter.Length - lastIndexOfPathDelimiter;
+            return urlWithOutProtocolDelimilter
+                .Substring(lastIndexOfPathDelimiter, lenghtOfPathOnly)
+                .Remove(0, 1);
+        }
+
+        private string GetPort(string url)
+        {
+            var indexOfPortDelimiter = url.LastIndexOf(":", StringComparison.CurrentCultureIgnoreCase);
+            var numberOfIndexsAllowedBasedOnDefaultProtocols = 5;
+            var port = indexOfPortDelimiter <= numberOfIndexsAllowedBasedOnDefaultProtocols
+                ? _defaultPorts[GetProtocol(url)]
+                : GetCustomPort(url);
+            return port;
+        }
+
+        private string GetCustomPort(string url)
+        {
+            return GetUrlsplit(url).Select(s =>
+            {
+                int.TryParse(s, out int result);
+                return result;
+            }).Sum().ToString();
+        }
+
+        private string GetDomain(string url)
+        {
+            var urlsplit = GetUrlsplit(url);
+            var domain = UrlContainsSubdomain(url) ? $"{urlsplit[2]}.{urlsplit[3]}" : $"{urlsplit[1]}.{urlsplit[2]}";
+            return domain;
+        }
+
+        private bool UrlContainsSubdomain(string url)
+        {
+            var indexEndOfProtocol = url.IndexOf("://", StringComparison.CurrentCultureIgnoreCase);
+            var numberOfProtocolDelimiter = 3;
+            var urlWithOutProtocol = url.Remove(0, indexEndOfProtocol + numberOfProtocolDelimiter);
+            var indexOfPortDelimiter = urlWithOutProtocol.IndexOf(":", StringComparison.CurrentCultureIgnoreCase);
+            var indexOfFileDirectoryDelimiter = urlWithOutProtocol.IndexOf("/", StringComparison.CurrentCultureIgnoreCase);
+            var endIndex = indexOfPortDelimiter >= 0 ? indexOfPortDelimiter : indexOfFileDirectoryDelimiter;
+            endIndex = endIndex != -1 ? endIndex : urlWithOutProtocol.Length;
+            var domainPossiblyWithSubdomain = urlWithOutProtocol.Substring(0, endIndex);
+            var numberOfDotsInDomain = domainPossiblyWithSubdomain.Count(c => c == '.');
+            return numberOfDotsInDomain == 2;
+        }
+
+        private string GetSubdomain(string url)
+        {
+            var urlsplit = GetUrlsplit(url);
+            var subdomain = UrlContainsSubdomain(url) ? urlsplit[1] : string.Empty;
+            return subdomain;
+        }
+
+        private string GetProtocol(string url)
+        {
+            var urlsplit = GetUrlsplit(url);
+            var protocol = urlsplit[0];
+            return protocol;
+        }
+
+        private string[] GetUrlsplit(string url)
+        {
+            var urlWithProtocol = url.Replace("://", ".");
+            var urlsplit = urlWithProtocol.Split(new[] { ".", ":", "/" }, StringSplitOptions.RemoveEmptyEntries);
+            return urlsplit;
+        }
+    }
+
+    public class UrlParts
+    {
+        public string Protocol { get; set; }
+        public string Subdomain { get; set; }
+        public string Domain { get; set; }
+        public string Port { get; internal set; }
+        public string Path { get; set; }
+    }
+}
---
+++ UrlParts/KataPlayGroundTemplate/UrlHelperTests.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KataPlayGroundTemplate
+{
+    [TestFixture]
+    public class UrlHelperTest
+    {
+        public class GetUrlParts
+        {
+            [Test]
+            public void GivenValidUrl_ShouldReturnUrlParts()
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                var url = "http://www.tddbuddy.com";
+                //Arrange
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                Assert.That(urlParts, Is.TypeOf<UrlParts>());
+            }
+
+            [Test]
+            [TestCase("http://www.tddbuddy.com", "http")]
+            [TestCase("https://www.buddy.com", "https")]
+            [TestCase("ftp://foo.com:9000/files", "ftp")]
+            [TestCase("ftps://foo.com:9000/files", "ftps")]
+            public void GivenUrl_ShouldReturnProtocol(string url, string protocol)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = protocol;
+                Assert.That(urlParts.Protocol, Is.EqualTo(expected));
+            }
+
+
+            [Test]
+            [TestCase("http://www.tddbuddy.com", "www")]
+            [TestCase("https://www.tddbuddy.com", "www")]
+            [TestCase("ftp://www.foo.com:9900/files", "www")]
+            [TestCase("ftps://www.foo.com:8500/files", "www")]
+            public void GivenUrlWithSubdomain_ShouldReturnSubdomain(string url, string subdomain)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = subdomain;
+                Assert.That(urlParts.Subdomain, Is.EqualTo(expected));
+            }
+
+            [Test]
+            [TestCase("http://tddbuddy.com")]
+            [TestCase("https://tddbuddy.com")]
+            [TestCase("ftp://foo.com:9900/files")]
+            [TestCase("ftps://foo.com/files")]
+            public void GivenUrlWithNoSubdomain_ShouldReturnEmptyString(string url)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = string.Empty;
+                Assert.That(urlParts.Subdomain, Is.EqualTo(expected));
+            }
+
+            [Test]
+            [TestCase("http://katas.tddbuddy.com", "katas")]
+            [TestCase("https://kata.tddbuddy.com", "kata")]
+            [TestCase("ftp://foo.zoo.com:9900/files", "foo")]
+            [TestCase("ftps://woo.too.com:8500/files", "woo")]
+            public void GivenUrlNotUsingWWWsubdomain_ShouldReturnSubdomain(string url, string subdomain)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = subdomain;
+                Assert.That(urlParts.Subdomain, Is.EqualTo(expected));
+            }
+
+            [Test]
+            [TestCase("http://tddbuddy.com", "tddbuddy.com")]
+            [TestCase("https://buddy.io", "buddy.io")]
+            [TestCase("ftp://foo.net:9900/files", "foo.net")]
+            [TestCase("ftps://qoo.biz:8500/files", "qoo.biz")]
+            public void GivenUrlWithNoSubdomain_ShouldReturnDomain(string url, string domain)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = domain;
+                Assert.That(urlParts.Domain, Is.EqualTo(expected));
+            }
+
+            [Test]
+            [TestCase("http://www.tddbuddy.com", "tddbuddy.com")]
+            [TestCase("https://www.buddy.io", "buddy.io")]
+            [TestCase("ftp://www.foo.net:9900/files", "foo.net")]
+            [TestCase("ftps://www.qoo.biz:8500/files", "qoo.biz")]
+            public void GivenUrl_ShouldWithSubdomainReturnDomain(string url, string domain)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = domain;
+                Assert.That(urlParts.Domain, Is.EqualTo(expected));
+            }
+
+            [Test]
+            [TestCase("http://www.tddbuddy.com", "80")]
+            [TestCase("https://www.buddy.io", "443")]
+            [TestCase("ftp://www.foo.net/files", "21")]
+            [TestCase("ftps://www.qoo.biz/files", "22")]
+            public void GivenUrlWithNoPortSpecfied_ShouldReturnDefaultPort(string url, string port)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = port;
+                Assert.That(urlParts.Port, Is.EqualTo(expected));
+            }
+
+            [Test]
+            [TestCase("http://www.tddbuddy.com:8080/test.html", "8080")]
+            [TestCase("https://www.buddy.io:444", "444")]
+            [TestCase("ftp://www.foo.net:9000", "9000")]
+            [TestCase("ftps://www.qoo.biz:9001/files", "9001")]
+            public void GivenUrlWithPortSpecfied_ShouldReturnDefaultPort(string url, string port)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = port;
+                Assert.That(urlParts.Port, Is.EqualTo(expected));
+            }
+
+            [Test]
+            [TestCase("http://www.tddbuddy.com")]
+            [TestCase("https://www.buddy.io")]
+            [TestCase("ftp://www.foo.net")]
+            [TestCase("ftps://www.qoo.biz")]
+            public void GivenUrlWithNoPath_ShouldReturnEmptyString(string url)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = string.Empty;
+                Assert.That(urlParts.Path, Is.EqualTo(expected));
+            }
+
+            [Test]
+            [TestCase("http://www.tddbuddy.com/foobar.html", "foobar.html")]
+            [TestCase("https://www.buddy.io/foo/bar.html", "foo/bar.html")]
+            [TestCase("ftp://www.foo.net/download/install.exe", "download/install.exe")]
+            [TestCase("ftps://www.qoo.biz/files", "files")]
+            public void GivenUrlWithPath_ShouldReturnEmptyString(string url, string path)
+            {
+                //Arrange
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = path;
+                Assert.That(urlParts.Path, Is.EqualTo(expected));
+            }
+
+            public void GivenHttpUrlFromExplain_ShouldReturnCorrectUrlParts()
+            {
+                //Arrange
+                var url= "http://www.tddbuddy.com";
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = new UrlParts()
+                {
+                    Protocol = "http",
+                    Subdomain = "www",
+                    Domain = "tddbuddy.com",
+                    Path = string.Empty,
+                    Port = "80"
+                };
+                Assert.That(urlParts, Is.EqualTo(expected));
+            }
+
+            public void GivenHttpUrlWithPathFromExplain_ShouldReturnCorrectUrlParts()
+            {
+                //Arrange
+                var url= "http://foo.bar.com/foobar.html";
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = new UrlParts()
+                {
+                    Protocol = "http",
+                    Subdomain = "foo",
+                    Domain = "bar.com",
+                    Path = "foobar.html",
+                    Port = "80"
+                };
+                Assert.That(urlParts, Is.EqualTo(expected));
+            }
+
+            public void GivenHttpsUrlWithPathFromExplain_ShouldReturnCorrectUrlParts()
+            {
+                //Arrange
+                var url= "https://www.foobar.com:8080/download/install.exe";
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = new UrlParts()
+                {
+                    Protocol = "https",
+                    Subdomain = "www",
+                    Domain = "foobar.com",
+                    Path = "download/install.exe",
+                    Port = "8080"
+                };
+                Assert.That(urlParts, Is.EqualTo(expected));
+            }
+            public void GivenFTPUrlWithPathFromExplain_ShouldReturnCorrectUrlParts()
+            {
+                //Arrange
+                var url= "ftp://foo.com:9000/files";
+                var urlHelper = UrlHelper();
+                //Act
+                var urlParts = urlHelper.GetUrlParts(url);
+                //Assert
+                var expected = new UrlParts()
+                {
+                    Protocol = "ftp",
+                    Subdomain = string.Empty,
+                    Domain = "foo.com",
+                    Path = "files",
+                    Port = "9000"
+                };
+                Assert.That(urlParts, Is.EqualTo(expected));
+            }
+        }
+        private static UrlHelper UrlHelper()
+        {
+            return new UrlHelper();
+        }
+    }
+}
